2063852dbf5210b6c47c888750cbd178882d6bd6
xmasterpiecex <a211019971420125@gmail.com>
2025-11-16 22:05:25 +0200

    [added message on client connection]

--- main.py
+++ main.py
@@ -10,47 +10,20 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 template = Jinja2Templates(directory="templates")
 
-clients = set()
-
-async def event_trigger(client_queue : Queue, req: Request):
-
-    while True:
-        client_message = await client_queue.get()
-        client_id = int(req.cookies.get("clientId", -1))
-        me = (client_id == client_message["id"])
-        safe_msg = client_message['message'].replace("\n", " ")
-
-        if me:
-            htmlEl = (
-                f"<div class='rightMsg'>"
-                f"<p class='rightTime'>{client_message['time']}</p>"
-                f"<p class='textMsg'>{safe_msg}</p></div>"
-            )
-
-        else:
-            htmlEl = (
-                f"<div class='leftMsg'>"
-                f"<p class='textMsg'>{safe_msg}</p>"
-                f"<p class='leftTime'>{client_message['time']}</p></div>"
-            )
-        response = (
-            "event:messageDelivered\n"
-            f"data: {htmlEl}\n\n"
-        )
-
-        yield response
+clients = {}
 
 @app.get("/events")
 async def events(req:Request):
     queue = Queue()
-    clients.add(queue)
+    clients[queue] = int(req.cookies.get("clientId", -1))
 
     async def stream():
         try:
-            async for msg_pkg in event_trigger(queue, req):
-                yield msg_pkg
+            while True:
+                mess = await queue.get()
+                yield mess
         finally:
-            clients.remove(queue)
+            clients.pop(queue)
             print(f"Client  disconnected")
 
     respons = StreamingResponse(stream(), media_type="text/event-stream", headers={
@@ -60,31 +33,41 @@ async def events(req:Request):
             },)
     return respons
 
+@app.get("/con")
+async def conn( req : Request):
+    text = "<----New client has connected---->"
+
+    for q in clients:
+        await q.put(f"event:clientConnected\ndata:{text}\n\n")
+    return {"status":200}
+
 @app.post("/pushMessage")
 async def message(req: Request, message: str=Form(...)):
 
-    dataPkg = {}
-    dataPkg["message"] = message
-    dataPkg[ "time" ] = datetime.now().strftime("%H:%M")
-    dataPkg["id"] = int( req.cookies['clientId'] )
+    id = int( req.cookies['clientId'] )
+    message = message.replace("\n", " ")
+    time = datetime.now().strftime("%H:%M")
 
-    for cl in clients:
-        await cl.put(dataPkg)
+    for que, clientId in clients.items():
+        me = (id == clientId)
+        if me:
+            htmlEl =f"<div class='rightMsg'><p class='rightTime'>{time}</p><p class='textMsg'>{message}</p></div>"
+        else:
+            htmlEl = f"<div class='leftMsg'><p class='textMsg'>{message}</p><p class='leftTime'>{time}</p></div>"
+
+        response = f"event:messageDelivered\ndata: {htmlEl}\n\n"
+        await que.put(response)
 
     return {"Status_Code": 200}
 
 @app.get("/", response_class=HTMLResponse)
 async def index(req: Request):
-
-    if "clientId" not in req.cookies:
-        clientId = random.randint(0,10000)
-    else:
-        clientId = int(req.cookies["clientId"])
+    clientId = req.cookies.get("clientId")
+    if not clientId:
+        clientId = str(random.randint(0,1000))
 
     respons = template.TemplateResponse(request=req, name="index.html")
-
-    if "clientId" not in req.cookies:
-        respons.set_cookie("clientId", str( clientId ))
+    respons.set_cookie("clientId", clientId)
 
     return respons
 
